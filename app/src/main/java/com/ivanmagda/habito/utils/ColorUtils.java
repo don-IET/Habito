a1c527778489f97f70ec08831154bd3bd9a4fc5f
Gáspár Vilmos <gaspar.vilmos24@gmail.com>
2017-04-25 17:09:31 +0200

    Unused local variable bugs fixed

--- app/src/main/java/com/ivanmagda/habito/utils/ColorUtils.java
+++ app/src/main/java/com/ivanmagda/habito/utils/ColorUtils.java
@@ -2,8 +2,9 @@ package com.ivanmagda.habito.utils;
 
 public final class ColorUtils {
 
-    public static void parseColorFromInt(int colorInt) {
+    public static String parseColorFromInt(int colorInt) {
         String hexString = Integer.toHexString(colorInt);
+        return  hexString;
     }
 
     private ColorUtils() {
