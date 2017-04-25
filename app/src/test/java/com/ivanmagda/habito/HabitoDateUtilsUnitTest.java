b52507b8ceb58bf4646b9613d86fc574950d38d1
ZoldMate <mate951006@gmail.com>
2017-04-25 16:28:13 +0200

    Java util Date class deprecated constructor fixed

--- app/src/test/java/com/ivanmagda/habito/HabitoDateUtilsUnitTest.java
+++ app/src/test/java/com/ivanmagda/habito/HabitoDateUtilsUnitTest.java
@@ -1,6 +1,8 @@
 package com.ivanmagda.habito;
 
 import com.ivanmagda.habito.utils.HabitoDateUtils;
+
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Test;
@@ -9,32 +11,48 @@ import static org.junit.Assert.*;
 public class HabitoDateUtilsUnitTest {
 
     @Test
-    public void isWithinRange_Date_test() throws Exception {
-        Date d = new Date(2017, 4, 25);
-        Date start = new Date(2017, 4, 1);
-        Date end = new Date(2017, 4, 30);
+    public void isWithinRange_Date_test()  {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 4, 25);
+        Date d = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        Date start = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 30);
+        Date end = calendar.getTime();
+
         assertTrue(HabitoDateUtils.isWithinRange(d, start, end));
     }
 
     @Test
-    public void isWithinRange_long_test() throws Exception {
-        Date d = new Date(2017, 4, 25);
-        Date start = new Date(2017, 4, 1);
-        Date end = new Date(2017, 4, 30);
+    public void isWithinRange_long_test(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 4, 25);
+        Date d = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        Date start = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 30);
+        Date end = calendar.getTime();
+
         assertTrue(HabitoDateUtils.isWithinRange(d.getTime(),start.getTime(),end.getTime()));
     }
 
     @Test
     public void isDateInCurrentWeek_test() {
-        Date d = new Date(2016, 4, 25);
-        assertFalse(HabitoDateUtils.isDateInCurrentWeek(d.getTime()));
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2016, 4, 25);
+        assertFalse(HabitoDateUtils.isDateInCurrentWeek(calendar.getTime().getTime()));
     }
 
     @Test
     public void isDatesInSameWeek_test(){
-        Date d1 = new Date(2017, 4, 25);
-        Date d2 = new Date(2017, 4, 25);
-        Date d3 = new Date(2017, 4, 1);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 4, 25);
+        Date d1 = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 26);
+        Date d2 = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        Date d3 = calendar.getTime();
+      
         assertTrue(HabitoDateUtils.isDatesInSameWeek(d1.getTime(), d2.getTime()));
         assertFalse(HabitoDateUtils.isDatesInSameWeek(d1.getTime(), d3.getTime()));
     }
