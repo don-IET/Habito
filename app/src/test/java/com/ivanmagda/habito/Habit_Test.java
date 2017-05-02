80df429533a3c42b29a28542e19acc37bec449b5
Szabolcs Kiss <k.szabolcs623@gmail.com>
2017-05-02 12:18:58 +0200

    Id getter test for Habit added

--- app/src/test/java/com/ivanmagda/habito/Habit_Test.java
+++ app/src/test/java/com/ivanmagda/habito/Habit_Test.java
@@ -14,4 +14,12 @@ public class Habit_Test {
         Habit h = new Habit();
         assertEquals(h.describeContents(),0);
     }
+
+    @Test
+    public void getId_Test() throws Exception {
+        Habit h = new Habit();
+        h.setId("11");
+        assertEquals(h.getId(),"11");
+    }
+
 }
